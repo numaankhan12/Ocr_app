5afc60e8872434a77084c67008531fabe3e31993
numaan <numaan@intelpeek.com>
2024-05-08 14:35:17 +0400

    added tesseract folder

--- app/OCR.py
+++ app/OCR.py
@@ -6,8 +6,9 @@ from pdf2image import convert_from_path
 from datetime import datetime, timedelta
 
 # Set the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract.exe"
+# pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract.exe"
 # pytesseract.pytesseract.tesseract_cmd = r'c:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"Tesseract-OCR\tesseract.exe"
 
 def process_image(image_path):
     try:
